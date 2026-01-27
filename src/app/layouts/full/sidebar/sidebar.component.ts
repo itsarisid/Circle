517ea618e4dc16892e0c0b63f307b789b092658a
Sajid Khan <khanalex301@gmail.com>
2026-01-27 07:33:33 +0300

    feat: Implement new sidebar component with collapse functionality and styling.

--- src/app/layouts/full/sidebar/sidebar.component.ts
+++ src/app/layouts/full/sidebar/sidebar.component.ts
@@ -9,10 +9,11 @@ import {
 import { BrandingComponent } from './branding.component';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [BrandingComponent, TablerIconsModule, MaterialModule],
+  imports: [BrandingComponent, TablerIconsModule, MaterialModule, CommonModule],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent implements OnInit {
